682cef29644d777e76fb00c621a33f60ad6d4b78
yhz <405296738@qq.com>
2021-01-15 16:49:10 +0800

    更新BaseActivity

--- yhz/src/main/java/com/yhz/yhz/activity/BaseActivity.java
+++ yhz/src/main/java/com/yhz/yhz/activity/BaseActivity.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Display;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
@@ -109,6 +110,51 @@ public abstract class BaseActivity extends AppCompatActivity implements NetBroad
             }
         }
         return super.onTouchEvent(event);
+    }
+
+    /**
+     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时则不能隐藏
+     *
+     * @param v
+     * @param event
+     * @return
+     */
+    private boolean isShouldHideKeyboard(View v, MotionEvent event)
+    {
+        if (v != null && (v instanceof EditText))
+        {  //判断得到的焦点控件是否包含EditText
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0],    //得到输入框在屏幕中上下左右的位置
+                    top = l[1],
+                    bottom = top + v.getHeight(),
+                    right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom)
+            {
+                // 点击位置如果是EditText的区域，忽略它，不收起键盘。
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+        // 如果焦点不是EditText则忽略
+        return false;
+    }
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     *
+     * @param token
+     */
+    private void hideKeyboard(IBinder token)
+    {
+        if (token != null)
+        {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
 
@@ -248,6 +294,7 @@ public abstract class BaseActivity extends AppCompatActivity implements NetBroad
                 View v = getCurrentFocus();
                 if (isShouldHideInput(v, ev)) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    hideKeyboard(v.getWindowToken());   //收起键盘
                     if (imm != null) {
                         imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                     }
