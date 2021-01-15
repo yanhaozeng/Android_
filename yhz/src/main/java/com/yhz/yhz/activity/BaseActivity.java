46e92d07f80f878b8d9b6a20579ee2abe34788b4
yhz <405296738@qq.com>
2021-01-15 17:00:54 +0800

    更新BaseActivity

--- yhz/src/main/java/com/yhz/yhz/activity/BaseActivity.java
+++ yhz/src/main/java/com/yhz/yhz/activity/BaseActivity.java
@@ -289,36 +289,58 @@ public abstract class BaseActivity extends AppCompatActivity implements NetBroad
     // 事件分发
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (isOpenKeyboardEvent) {
-            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-                View v = getCurrentFocus();
-                if (isShouldHideInput(v, ev)) {
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    hideKeyboard(v.getWindowToken());   //收起键盘
-                    if (imm != null) {
-                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                    }
+//        if (isOpenKeyboardEvent) {
+//            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+//                View v = getCurrentFocus();
+//                if (isShouldHideInput(v, ev)) {
+//                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//                    hideKeyboard(v.getWindowToken());   //收起键盘
+//                    if (imm != null) {
+//                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+//                    }
+//                }
+//                return super.dispatchTouchEvent(ev);
+//            }
+//            // 必不可少，否则所有的组件都不会有TouchEvent了
+//            return getWindow().superDispatchTouchEvent(ev) || onTouchEvent(ev);
+//        } else {
+//            return super.dispatchTouchEvent(ev);
+//        }
+
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
-                return super.dispatchTouchEvent(ev);
             }
-            // 必不可少，否则所有的组件都不会有TouchEvent了
-            return getWindow().superDispatchTouchEvent(ev) || onTouchEvent(ev);
-        } else {
             return super.dispatchTouchEvent(ev);
         }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
     }
 
-    private boolean isShouldHideInput(View v, MotionEvent event) {
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
         if (v != null && (v instanceof EditText)) {
-            int[] leftTop = {0, 0};
-            // 获取输入框当前的location位置
+            int[] leftTop = { 0, 0 };
+            //获取输入框当前的location位置
             v.getLocationInWindow(leftTop);
             int left = leftTop[0];
             int top = leftTop[1];
             int bottom = top + v.getHeight();
             int right = left + v.getWidth();
-            return !(event.getX() > left) || !(event.getX() < right)
-                    || !(event.getY() > top) || !(event.getY() < bottom);
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
         }
         return false;
     }
